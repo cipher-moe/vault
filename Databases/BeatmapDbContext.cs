bb7212e4fecdb71c5f95e6f55e62ede5c1d0ed29
minhducsun2002 <32769878+minhducsun2002@users.noreply.github.com>
2022-01-10 17:28:09 +0700

    Make beatmap cache static

--- Databases/BeatmapDbContext.cs
+++ Databases/BeatmapDbContext.cs
@@ -11,8 +11,8 @@ namespace vault.Databases
     {
         private readonly OsuClient osuClient;
         private readonly HttpClient httpClient;
-        private readonly FastConcurrentLru<string, Beatmap> cache = new(500);
-        private readonly FastConcurrentLru<string, string> idToHashCache = new(500);
+        private static readonly FastConcurrentLru<string, Beatmap> Cache = new(500);
+        private static readonly FastConcurrentLru<string, string> IDToHashCache = new(500);
 
         public BeatmapDbContext(DbContextOptions<BeatmapDbContext> options, OsuClient osuClient, HttpClient httpClient) : base(options)
         {
@@ -29,7 +29,7 @@ namespace vault.Databases
         
         public async Task<string?> GetBeatmapHash(string id)
         {
-            if (idToHashCache.TryGet(id, out var hash)) return hash;
+            if (IDToHashCache.TryGet(id, out var hash)) return hash;
             try
             {
                 var stream = await httpClient.GetStreamAsync("https://osu.ppy.sh/osu/" + id);
@@ -41,15 +41,15 @@ namespace vault.Databases
             {
                 return null;
             }
-            idToHashCache.AddOrUpdate(id, hash);
+            IDToHashCache.AddOrUpdate(id, hash);
             return hash;
         }
         
         public async Task<Beatmap?> GetByHash(string hash)
         {
-            if (cache.TryGet(hash, out var @return)) return @return;
+            if (Cache.TryGet(hash, out var @return)) return @return;
             var res = await osuClient.GetBeatmapByHashAsync(hash);
-            cache.AddOrUpdate(hash, res);
+            Cache.AddOrUpdate(hash, res);
             return res;
         } 
     }
